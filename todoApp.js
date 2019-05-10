3ad873a21e9a305916627787d9e8437fde751ae0
Vera Vovk <vovkvera@gmail.com>
2019-05-10 09:08:32 +0300

    Update todoApp.js

--- todoApp.js
+++ todoApp.js
@@ -1,84 +1,82 @@
 // https://snack.expo.io/@veravovk/todoapp
 import React from 'react';
-import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
-import {Constants} from 'expo'
-
-let id = 0
+import {View, Button, Text, StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
-  todoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
   appContainer: {
-    paddingTop: Constants.statusBarHeight,
-  },
-  fill: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  count: {
+    fontSize: 48,
   }
 })
 
-const Todo = props => (
-  <View style={styles.todoContainer}> 
-    <Switch value={props.todo.checked} onValueChange={props.onToggle} />
-    <Button onPress={props.onDelete} title="delete" />
-    <Text>{props.todo.text}</Text>
-  </View>
-)
-
-export default class App extends React.Component {
+class Counter extends React.Component {
   constructor() {
     super()
     this.state = {
-      todos: [],
+      count: 0,
     }
   }
-
-  addTodo() {
-    id++
-    const text = ` Задача № ${id}`
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {id: id, text: text, checked: false},
-      ], 
-    })
+  
+  componentDidMount() {
+    this.interval = setInterval(this.inc, 1000)
   }
-
-  removeTodo(id) {
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
-    })
+  
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
-
-  toggleTodo(id) {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id !== id) return todo
-        return {
-          id: todo.id,
-          text: todo.text,
-          checked: !todo.checked,
-        }
-      })
-    })
+  
+  inc = () => {
+    console.log('increment!')
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }))
   }
 
   render() {
     return (
-      <View style={[styles.appContainer, styles.fill]}>
-        <Text>Todo count: {this.state.todos.length}</Text>
-        <Text>Unchecked todo count: {this.state.todos.filter(todo => !todo.checked).length}</Text>
-        <Button onPress={() => this.addTodo()} title="Добавить задачку" />
-        <ScrollView style={styles.fill}>
-          {this.state.todos.map(todo => (
-            <Todo
-              onToggle={() => this.toggleTodo(todo.id)}
-              onDelete={() => this.removeTodo(todo.id)}
-              todo={todo}
-            />
-          ))}
-        </ScrollView>
+      <Text style={styles.count}>{this.state.count} </Text>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCounter: true,
+    }
+  }
+  
+  toggleCounter = () => this.setState(prevState => ({
+    showCounter: !prevState.showCounter,
+  }))
+
+  render() {
+    if (this.state.showCounter) {
+      return (
+        <View style={styles.appContainer}>
+          <Button title="toggle" onPress={this.toggleCounter} />
+          <Counter />
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.appContainer}>
+          <Button title="toggle" onPress={this.toggleCounter} />
+        </View>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Button title="toggle" onPress={this.toggleCounter} />
+        {this.state.showCounter && <Counter />}
       </View>
     )
   }
