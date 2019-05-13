d42ca775d2aa1f1baa812b9719962f81e6f6efc9
Vera Vovk <vovkvera@gmail.com>
2019-05-13 08:43:42 +0300

    Update objMut.js

--- objMut.js
+++ objMut.js
@@ -1,3 +1,72 @@
+import React from 'react';
+import {View, Button, Text, StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  count: {
+    fontSize: 48,
+  }
+})
+
+class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+    }
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(this.inc, 1000)
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  
+  inc = () => {
+    console.log('increment!')
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }))
+  }
+
+  render() {
+    return (
+      <Text style={styles.count}>{this.state.count} </Text>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCounter: true,
+    }
+  }
+  
+  toggleCounter = () => this.setState(prevState => ({
+    showCounter: !prevState.showCounter,
+  }))
+
+
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Button title="toggle" onPress={this.toggleCounter} />
+        {this.state.showCounter && <Counter />}
+      </View>
+    )
+  }
+}
+
+///////////
+
 const o = {
   a: 'a',
   b: 'b',
@@ -73,3 +142,5 @@ sayHello()
 const d = new Date();
 d.toString()
 //
+
+
