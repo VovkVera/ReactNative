70e67ed85ac0b941dad1e6054216689db801f84d
Vera Vovk <vovkvera@gmail.com>
2019-05-16 07:19:56 +0300

    Update todoApp.js

--- todoApp.js
+++ todoApp.js
@@ -1,95 +1,59 @@
 // https://snack.expo.io/@veravovk/todoapp
-import React from 'react';
-import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
+import React from 'react'
+import {Button, StyleSheet, TextInput, View} from 'react-native'
 import {Constants} from 'expo'
 
-let id = 0
-
 const styles = StyleSheet.create({
-  todoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  appContainer: {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
     paddingTop: Constants.statusBarHeight,
   },
-  fill: {
-    flex: 1,
-  }
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    minWidth: 100,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 3,
+  },
 })
 
-const Todo = props => (
-  <View style={styles.todoContainer}>
-    <Switch value={props.todo.checked} onValueChange={props.onToggle} />
-    <Button onPress={props.onDelete} title="delete" />
-    <Text>{props.todo.text}</Text>
-  </View>
-
-
-)
-
-export default class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: [],
-      
-    }
-    
+export default class AddContactForm extends React.Component {
+  state = {
+    name: '',
+    phone: '',
+    isFormValid: false,
   }
 
-  addTodo() {
-    id++
-    const text = `TODO number ${id}`
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {id: id, text: text, checked: false},
-      ], 
-    })
+  handleNameChange = name => {
+    this.setState({name})
   }
 
-  removeTodo(id) {
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
-    })
-  }
-
-  toggleTodo(id) {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id !== id) return todo
-        return {
-          id: todo.id,
-          text: todo.text,
-          checked: !todo.checked,
-        }
-      })
-    })
+  handlePhoneChange = phone => {
+    this.setState({phone})
   }
 
   render() {
     return (
-      <View style={[styles.appContainer, styles.fill]}>
-        <Text>Todo count: {this.state.todos.length}</Text>
-        <Text>Unchecked todo count: {this.state.todos.filter(todo => !todo.checked).length}</Text>
-        <Button onPress={() => this.addTodo()} title="Add TODO" />
-        <Text>Привет! </Text>
-        
-        <ScrollView style={styles.fill}>
-          {this.state.todos.map(todo => (
-            <Todo
-              onToggle={() => this.toggleTodo(todo.id)}
-              onDelete={() => this.removeTodo(todo.id)}
-              todo={todo}
-            />
-            
-          ))}
-        </ScrollView>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          value={this.state.name}
+          onChangeText={this.handleNameChange}
+          placeholder="Name"
+        />
+        <TextInput
+          keyboardType="numeric"
+          style={styles.input}
+          value={this.state.phone}
+          onChangeText={this.handlePhoneChange}
+          placeholder="Phone"
+        />
+        <Button title="Submit" />
       </View>
     )
   }
 }
-
-
-
